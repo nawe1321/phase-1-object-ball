668dfd54194a93c73acfdb69c3e5fdb512c1f8c1
Nathan <nathan.wendlowsky@flatironschool.com>
2023-07-14 14:03:29 -0700

    Completed with instructor 1:1 assistance for debugger

--- src/02-advanced-debug.js
+++ src/02-advanced-debug.js
@@ -16,13 +16,127 @@ function goodPractices() {
 
       // what is 'data' at each loop through out this block?
       // when will the following line of code work and when will it break?
-      let data = teamObj.player
+      let data = teamObj.players
       for (let key in data) {
         debugger
       }
     }
   }
 }
+function numPointsScored(playerName) {
+  let game = gameObject();
+  for (let gameKey in game) {
+    debugger;
+    let teamObj = game[gameKey]
+    for (let teamKey in teamObj) {
+      debugger;
+      if (teamKey === 'players') {
+        for (let playerKey in teamObj[teamKey]) {
+          debugger;
+          if (playerKey === playerName) {
+            debugger;
+            return teamObj[teamKey][playerKey].points;
+          }
+        }
+      }
+      
+    }
+  }
+}
+
+function shoeSize(playerName) {
+  let game = gameObject();
+  for (let gameKey in game) {
+    debugger;
+    let teamObj = game[gameKey];
+    for (let teamKey in teamObj) {
+      if (teamKey === "players") {
+        for (let playerKey in teamObj[teamKey]) {
+          debugger;
+          if (playerKey === playerName) {
+            return teamObj[teamKey][playerKey].shoe;
+          }
+        }
+      }
+    }
+  }
+}
+
+function teamColors(teamName) {
+  let game = gameObject();
+  for (let gameKey in game) {
+    let teamObj = game[gameKey];
+    if (teamObj.teamName === teamName) {
+      debugger;
+      return teamObj.colors;
+    }
+  }
+}
+
+function teamNames() {
+  let game = gameObject();
+  debugger;
+  return [game.home.teamName, game.away.teamName];
+}
+
+function playerNumbers(teamName) {
+  let game = gameObject();
+  for (let gameKey in game) {
+    let teamObj = game[gameKey];
+    if (teamObj.teamName === teamName) {
+      let numbers = [];
+      for (let playerKey in teamObj.players) {
+        numbers.push(teamObj.players[playerKey].number);
+      }
+      debugger;
+      return numbers;
+    }
+  }
+}
+
+function playerStats(playerName) {
+  let game = gameObject();
+  for (let gameKey in game) {
+    let teamObj = game[gameKey];
+    for (let teamKey in teamObj) {
+      if (teamKey === "players") {
+        let playerStats = teamObj[teamKey][playerName];
+        debugger;
+        return playerStats;
+      }
+    }
+  }
+}
+
+function bigShoeRebounds() {
+  let game = gameObject();
+  let largestShoeSize = 0;
+  let playerWithLargestShoe = "";
+  for (let gameKey in game) {
+    let teamObj = game[gameKey];
+    for (let teamKey in teamObj) {
+      if (teamKey === "players") {
+        for (let playerKey in teamObj[teamKey]) {
+          let player = teamObj[teamKey][playerKey];
+          if (player.shoe > largestShoeSize) {
+            largestShoeSize = player.shoe;
+            playerWithLargestShoe = playerKey;
+          }
+        }
+      }
+    }
+  }
+  debugger;
+  return game.home.players[playerWithLargestShoe].rebounds || game.away.players[playerWithLargestShoe].rebounds;
+}
+
 
 // then, call the function so it runs!
-goodPractices()
+// goodPractices()
+// numPointsScored("Alan Anderson")
+// shoeSize("Alan Anderson")
+// teamColors("Brooklyn Nets")
+// teamNames()
+// playerNumbers("Brooklyn Nets")
+// playerStats("Alan Anderson")
+bigShoeRebounds()
